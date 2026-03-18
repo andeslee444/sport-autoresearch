9b77c090702b12182ef53e0c682fdb6311fc862a
andeslee444 <harbortheoctopus@gmail.com>
2026-03-18 10:24:41 -0400

    exp: offsets [-20..20]

--- experiment.py
+++ experiment.py
@@ -36,7 +36,7 @@ USE_STAT_SPACE = False
 # Testing scope: line offsets and extended stat types
 # TEST_LINE_OFFSETS = [0] tests only at training-set mean
 # TEST_LINE_OFFSETS = [-2, 0, 2] tests at mean-2, mean, mean+2 (3x samples)
-TEST_LINE_OFFSETS = [-15, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 15]
+TEST_LINE_OFFSETS = [-20, -15, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12, 15, 20]
 
 # USE_EXTENDED_STATS = True adds steals, blocks, turnovers, rebounds+assists
 USE_EXTENDED_STATS = True
