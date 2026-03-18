bea854563bdf34de05b83bf79297770854b65444
andeslee444 <harbortheoctopus@gmail.com>
2026-03-18 10:22:32 -0400

    exp: VENUE_CAP=0.06

--- experiment.py
+++ experiment.py
@@ -15,7 +15,7 @@ MATCHUP_CAP = 0.20
 # Venue: home/away performance shift
 # Formula: clamp((venue_avg / season_avg - 1.0) * MULTIPLIER, -CAP, CAP)
 VENUE_MULTIPLIER = 0.30
-VENUE_CAP = 0.08
+VENUE_CAP = 0.06
 
 # Back-to-back: second night penalty (fixed)
 B2B_PENALTY = 0.0
